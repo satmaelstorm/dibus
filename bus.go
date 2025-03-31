4b07681f90419c8bd9814ba534b093c20b43546d
Vlad S <s....m@gmail.com>
2025-03-31 21:35:15 +0000

    bus stop command

--- bus.go
+++ bus.go
@@ -14,21 +14,27 @@ type initSubscribersListItem struct {
 	order int64
 }
 
+type BusOptions struct {
+	AwaitForGracefulStop time.Duration
+}
+
 type ApplicationBus struct {
 	subscribers      map[EventName][]Subscriber
 	needAwaitStops   []<-chan struct{}
 	awaitStopTimeout time.Duration
 	ctx              context.Context
 	cancel           context.CancelFunc
+	signalChannel    chan os.Signal
 }
 
-func NewApplicationBus(ctx context.Context, awaitForGracefulStop time.Duration) *ApplicationBus {
+func NewApplicationBus(ctx context.Context, opts BusOptions) *ApplicationBus {
 	ctxInner, cancel := context.WithCancel(ctx)
 	return &ApplicationBus{
 		subscribers:      make(map[EventName][]Subscriber),
-		awaitStopTimeout: awaitForGracefulStop,
+		awaitStopTimeout: opts.AwaitForGracefulStop,
 		ctx:              ctxInner,
 		cancel:           cancel,
+		signalChannel:    make(chan os.Signal, 1),
 	}
 }
 
@@ -55,6 +61,7 @@ func (ab *ApplicationBus) ExecCommand(command Command) {
 }
 
 func (ab *ApplicationBus) Build(providers ...SubscriberProvider) {
+	ab.selfSubscribe()
 	initList := make([]initSubscribersListItem, len(providers))
 	for idx, provider := range providers {
 		subscriber := provider(ab.ctx, ab)
@@ -75,6 +82,11 @@ func (ab *ApplicationBus) Build(providers ...SubscriberProvider) {
 	ab.init(initList)
 }
 
+func (ab *ApplicationBus) selfSubscribe() {
+	stopEvent := &BusStopCommand{}
+	ab.subscribers[stopEvent.Name()] = []Subscriber{ab}
+}
+
 func (ab *ApplicationBus) init(list []initSubscribersListItem) {
 	sort.Slice(list, func(i, j int) bool {
 		return list[i].order < list[j].order
@@ -85,31 +97,11 @@ func (ab *ApplicationBus) init(list []initSubscribersListItem) {
 }
 
 func (ab *ApplicationBus) Run() {
-	signalChannel := make(chan os.Signal, 1)
-	signal.Notify(signalChannel, os.Interrupt, syscall.SIGTERM)
+	signal.Notify(ab.signalChannel, os.Interrupt, syscall.SIGTERM)
 	go func(ch <-chan os.Signal) {
 		<-ch
-		ab.cancel()
-
-		ctx, cancel := context.WithTimeout(context.Background(), ab.awaitStopTimeout)
-		defer cancel()
-
-		allStopped := make(chan struct{})
-
-		go func() {
-			for _, chnl := range ab.needAwaitStops {
-				<-chnl
-			}
-			allStopped <- struct{}{}
-		}()
-
-		select {
-		case <-allStopped:
-			return
-		case <-ctx.Done():
-			return
-		}
-	}(signalChannel)
+		ab.shutdown()
+	}(ab.signalChannel)
 
 	<-ab.ctx.Done()
 }
@@ -117,4 +109,42 @@ func (ab *ApplicationBus) Run() {
 func (ab *ApplicationBus) BuildAndRun(providers ...SubscriberProvider) {
 	ab.Build(providers...)
 	ab.Run()
+}
+
+func (ab *ApplicationBus) shutdown() {
+	ab.cancel()
+	ctx, cancel := context.WithTimeout(context.Background(), ab.awaitStopTimeout)
+	defer cancel()
+	defer close(ab.signalChannel)
+
+	allStopped := make(chan struct{})
+
+	go func() {
+		for _, chnl := range ab.needAwaitStops {
+			<-chnl
+		}
+		allStopped <- struct{}{}
+	}()
+
+	select {
+	case <-allStopped:
+		return
+	case <-ctx.Done():
+		return
+	}
+}
+
+func (ab *ApplicationBus) ProcessQuery(query Query) Query {
+	return query
+}
+
+func (ab *ApplicationBus) ProcessCommand(command Command) {
+	switch command.(type) {
+	case *BusStopCommand:
+		ab.shutdown()
+	}
+}
+
+func (ab *ApplicationBus) IamStopChan() <-chan struct{} {
+	return nil
 }
